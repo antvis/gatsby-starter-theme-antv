96811e4835591f845c8098ea6898325c2ec5030d
afc163 <afc163@gmail.com>
2019-12-02 15:08:04 +0800

    :zap: Add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,55 +2,55 @@ module.exports = {
   plugins: [
     {
       resolve: '@antv/gatsby-theme-antv',
-      options: {},
-    },
+      options: {}
+    }
   ],
   siteMetadata: {
     title: 'AntV',
     description: 'Gatsby starter of antv theme',
-    siteUrl: 'https://gatsby-starter-theme-antv.antv.vision/',
+    siteUrl: 'https://gatsby-starter-theme-antv.antv.vision',
     githubUrl: 'https://github.com/antvis/antvis.github.io',
     navs: [
       {
         slug: 'docs/specification/getting-started',
         title: {
           zh: '设计语言',
-          en: 'Specification',
-        },
+          en: 'Specification'
+        }
       },
       {
         slug: 'examples',
         title: {
           zh: '图表演示',
-          en: 'Examples',
-        },
+          en: 'Examples'
+        }
       },
       {
         slug: 'independent',
         title: {
           zh: '独立',
-          en: 'MyApp',
-        },
+          en: 'MyApp'
+        }
         // target: '_blank',
-      },
+      }
     ],
     docs: [
       {
         slug: 'specification/category',
         title: {
           zh: '分类一',
-          en: 'category1',
+          en: 'category1'
         },
-        order: 4,
+        order: 4
       },
       {
         slug: 'specification/category/three',
         title: {
           zh: '第三层',
-          en: 'three level',
+          en: 'three level'
         },
-        order: 2,
-      },
+        order: 2
+      }
     ],
     examples: [
       {
@@ -58,15 +58,15 @@ module.exports = {
         icon: 'pie',
         title: {
           zh: '饼图分类',
-          en: 'Category',
-        },
-      },
+          en: 'Category'
+        }
+      }
     ],
     playground: {
       container: '<div id="container" />',
       playgroundDidMount: 'console.log("playgroundDidMount");',
-      playgroundWillUnmount: 'console.log("playgroundWillUnmount");',
+      playgroundWillUnmount: 'console.log("playgroundWillUnmount");'
     },
-    redirects: [],
-  },
+    redirects: []
+  }
 };
