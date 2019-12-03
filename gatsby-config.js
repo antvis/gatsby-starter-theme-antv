5dc66c3bcd2bc4d5f53ebb056fd4b5228f5133fe
afc163 <afc163@gmail.com>
2019-12-03 16:53:39 +0800

    :wrench: support more options

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,11 @@ module.exports = {
         }
       }
     ],
+    showSearch: false, // 是否展示搜索框
+    showChinaMirror: false, // 是否展示国内镜像链接
+    showAntVProductsCard: false, // 是否展示 AntV 系列产品的卡片链接
+    showLanguageSwitcher: true, // 用于定义是否展示语言切换
+    showGithubCorner: true, // 是否展示角落的 GitHub 图标
     playground: {
       container: '<div id="container" />',
       playgroundDidMount: 'console.log("playgroundDidMount");',
